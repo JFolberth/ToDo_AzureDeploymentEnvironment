ae985cfb8bda1042aa714a8e6821b284cfa7f97b
John Folberth <johnfolberth@gmail.com>
2023-10-28 22:41:04 -0400

    update reference to role id

--- infrastructure/terraform/modules/cosmosSqlRoleAssignment/main.tf
+++ infrastructure/terraform/modules/cosmosSqlRoleAssignment/main.tf
@@ -6,11 +6,17 @@ data "azurerm_cosmosdb_account" "cosmosdb_account" {
   resource_group_name = var.cosmosdb_account_resource_group
 }
 
+data "azurerm_cosmosdb_sql_role_definition" "cosmosdb_role_definition" {
+  resource_group_name = var.cosmosdb_account_resource_group
+  account_name        = var.cosmosdb_account_name
+  role_definition_id  = var.cosmosdb_account_role_definition
+}
+
 resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_sql_assignment" {
   name                = random_uuid.role_assignment_id.result
   resource_group_name = var.cosmosdb_account_resource_group
   account_name        = var.cosmosdb_account_name
-  role_definition_id  = var.cosmosdb_account_role_definition
+  role_definition_id  = data.azurerm_cosmosdb_sql_role_definition.cosmosdb_role_definition.id
   principal_id        = var.principal_id
   scope               = data.azurerm_cosmosdb_account.cosmosdb_account.id
 }
