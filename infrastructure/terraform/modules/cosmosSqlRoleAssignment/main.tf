acc408e5344d117511aee80dec99a995b7cc4f36
John Folberth <johnfolberth@gmail.com>
2023-10-28 22:17:56 -0400

    update scope

--- infrastructure/terraform/modules/cosmosSqlRoleAssignment/main.tf
+++ infrastructure/terraform/modules/cosmosSqlRoleAssignment/main.tf
@@ -12,5 +12,5 @@ resource "azurerm_cosmosdb_sql_role_assignment" "cosmosdb_sql_assignment" {
   account_name        = var.cosmosdb_account_name
   role_definition_id  = var.cosmosdb_account_role_definition
   principal_id        = var.principal_id
-  scope               = data.azurerm_cosmosdb_account.cosmosdb_account
+  scope               = data.azurerm_cosmosdb_account.cosmosdb_account.id
 }
