9a0a29f43b1ea89d793f9c3adf20cef64dc5bb0d
John Folberth <johnfolberth@gmail.com>
2025-02-06 14:01:12 -0500

    Update azurerm provider version constraint and skip provider registration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,13 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.71, < 5.0.0"
+      version = "~>3.2"
     }
   }
 }
 
 provider "azurerm" {
+
   features {}
+  skip_provider_registration = "true"
 }
 
 provider "random" {}
