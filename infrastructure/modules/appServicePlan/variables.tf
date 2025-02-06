6465596650ce08734cbd0a0730c5fa6e5c928e30
John Folberth <johnfolberth@gmail.com>
2025-02-06 14:29:51 -0500

    Update default SKU for App Service Plan to B1 in variables.tf

--- infrastructure/modules/appServicePlan/variables.tf
+++ infrastructure/modules/appServicePlan/variables.tf
@@ -26,7 +26,7 @@ variable "service_plan_os_type" {
 variable "service_plan_sku_name" {
   type        = string
   description = "SKU for the App Service Plan"
-  default     = "D1"
+  default     = "B1"
   validation {
     condition     = contains(["B1", "B2", "B3", "D1", "F1", "I1", "I2", "I3", "I1v2", "I2v2", "I3v2", "P1v2", "P2v2", "P3v2", "P1v3", "P2v3", "P3v3", "S1", "S2", "S3", "SHARED", "EP1", "EP2", "EP3", "WS1", "WS2", "WS3"], var.service_plan_sku_name)
     error_message = "Please provide a valid App Serice Plan SKU"
